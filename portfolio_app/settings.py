e9d356161449fb8e67c4d4fb7132b5ff84f041b2
waregagbagbo <gabelwarega@gmail.com>
2023-04-30 16:46:53 +0300

    fixed the templates and views problem

--- portfolio_app/settings.py
+++ portfolio_app/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'portfolio_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':os.path.join [{'templates'}],
+        'DIRS':['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
