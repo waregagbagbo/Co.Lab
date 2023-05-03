b06a075cecdedc168936fe575db896044d98c347
waregagbagbo <gabelwarega@gmail.com>
2023-05-03 18:11:13 +0300

    integrated weather api

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,6 @@
+import json
 from django.shortcuts import render, HttpResponse
-# Create your views here.
+import urllib.request
 # create the views
 
 def index(request): # home view
@@ -18,8 +19,32 @@ def contact(request):
     return render(request,'pages/contact.html')
 
 
-def podcasts(request):
-    return render(request,'pages/podcasts.html')
-
+def podcasts(request): 
+    if request.method == 'POST':
+        # get the city name fro the api
+        city = request.POST.get('London','True')
+        
+        # fetch informationusing the API
+        source = urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=3b8dc39f762c805cedd65b91051b5190").read()
+  
+        # convert json data file into python dict
+        list_of_data = json.loads(source)
+        
+        # create dict and convert to string
+        context ={
+            'city': city,
+            'country_code':str(list_of_data['sys']['country']),
+            'coordinate': str(list_of_data['coord']['lon'])+ ''+ str(list_of_data['coord']['lat']),
+            'temp': str(list_of_data['main']['temp']) + 'k',
+            'pressure': str(list_of_data['main']['pressure']),
+            'humidity': str(list_of_data['main']['humidity']),            
+            
+        }
+    else:
+        context ={}
+        
+    # push context dta to the template
+    return render(request,'pages/podcasts.html', context)
+        
 
 
