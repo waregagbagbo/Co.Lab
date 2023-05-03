174eaea5da5a30ec9b6bd0e64739c922f0090edb
waregagbagbo <gabelwarega@gmail.com>
2023-05-03 15:52:43 +0300

    Added newsapi library via pip

--- portfolio_app/settings.py
+++ portfolio_app/settings.py
@@ -116,16 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # path to local dev image storage
+STATIC_ROOT = os.path.join(BASE_DIR,'static') # development image directory
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-STATIC_URL = '/static/'  # path to local dev image storage
-STATIC_ROOT = os.path.join(BASE_DIR,'static') # static is for  development image directory
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-#from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.contrib.staticfiles.urls import static
+from django.conf.urls.static import static
+#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+#from django.contrib.staticfiles.urls import static
 
 
 
@@ -28,5 +28,10 @@ urlpatterns = [
     path('', include('portfolio.urls')),# add app.urls
     
 ]
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+
+
+#urlpatterns += staticfiles_urlpatterns()
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
