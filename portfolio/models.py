863164a2d8d722c482b2f1187aac589854012a2f
waregagbagbo <gabelwarega@gmail.com>
2023-05-06 03:56:07 +0300

    created db migrations of the contacts model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# create contact form model
+
+class Contact(models.Model):
+    name = models.CharField(max_length=20, null=True)
+    email = models.EmailField(max_length=20)
+    message = models.TextField(blank=False, null=True, max_length=100)
+    
+    class Meta:
+        verbose_name = 'Contact'
+        
+    def __init__(self):
+        return self.name, self.email
