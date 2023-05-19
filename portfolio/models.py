b7a862de9e6bdd749e12303744653f3437097b9d
waregagbagbo <gabelwarega@gmail.com>
2023-05-19 14:16:08 +0300

    created projects model and added pillow library

--- portfolio/models.py
+++ portfolio/models.py
@@ -12,3 +12,14 @@ class Contact(models.Model):
     def __str__(self):
         #return self.name
         return '%s %s' % (self.name, self.email)
+
+class Projects(models.Model):
+    title = models.CharField(max_length=20, null=True)
+    description = models.CharField(max_length=30, null=True)
+    image = models.ImageField(upload_to='static/images/', null=True)
+    
+    class Meta:
+        verbose_name = 'Projects'
+        
+    def __str__(self):
+        return '%s %s' % (self.title, self.description)
