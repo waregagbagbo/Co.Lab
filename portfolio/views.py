a0f6da1db70e1ee9e86adf87533c45eb631d0045
waregagbagbo <gabelwarega@gmail.com>
2023-05-20 17:12:34 +0300

    added jokesdad api

--- portfolio/views.py
+++ portfolio/views.py
@@ -6,14 +6,14 @@ from .forms import ContactMeForm
 from .models import Contact, Project
 from django.contrib import messages # for messaging
 import requests
-from jokeapi import Jokes
-import asyncio
+
 
 
 # create the views
 def index(request):
+    queryset = Project.objects.all()
     if request.method == 'POST':
-        # create an instance of the class/object
+        # create an instance of the class/object 
         form = ContactMeForm(request.POST)
         # check validity of the object form
         if form.is_valid():
@@ -26,38 +26,17 @@ def index(request):
     return render(request,'pages/index.html',{'form':form})
 
 
-def tools(request): # projects view
-    Projects = Project.objects.all()
-    
-    return render(request,'pages/index.html')
 
-
-def contact(request):
-    # get / initializesource  a post request from the user
-    '''if request.method == 'POST':
-        # create an instance of the class/object
-        form = ContactMeForm(request.POST)
-        # check validity of the object form
-        if form.is_valid():
-            # do something
-            form.save()
-            return HttpResponseRedirect('Thank you')
-    else:
-        form = ContactMeForm() # returns a GET request
-    return render(request,'pages/contact.html', {'form':form})'''
-
-
-async def jokes(request):
-    j = await Jokes()  # Initialise the class
-    jok = await j.get_joke(category=['programming', 'christmas','dark'])  # Will return a joke that fits in either the programming or dark category.
-    if jok["type"] == "single": # Print the joke
-       print(jok["jok"])
-    else:
-        print(jok["setup"])
-        print(jok["delivery"])    
-    response = asyncio.run(jokes())        
+def jokes(request):
+    url ="https://dad-jokes.p.rapidapi.com/random/joke"
+    headers = {
+	"X-RapidAPI-Key": "8ad0683ba1msh4c14b47abe52a3bp1608f0jsn7fcfea9b9564",
+	"X-RapidAPI-Host": "dad-jokes.p.rapidapi.com"
+ }
+    response = requests.get(url, headers=headers)
+    final_response = print(response.json())           
     context ={
-        "response":response,
+        "final":final_response,
     }    
     return render(request,'pages/jokes.html',context)
      
