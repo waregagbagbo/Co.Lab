9288d74a1c4e5b5982da8fca649dfc4517fe6375
waregagbagbo <gabelwarega@gmail.com>
2023-07-03 15:49:05 +0300

    updated databse conection

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -20,9 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 #from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 #from django.contrib.staticfiles.urls import static
-
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),# add app.urls
